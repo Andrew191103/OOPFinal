2d8c18cf778c3449233dd482bbdadfb96a0e3176
Andrew191103 <114073048+Andrew191103@users.noreply.github.com>
2023-06-06 00:31:17 +0700

    Update MangaInfo.java

--- MangaInfo.java
+++ MangaInfo.java
@@ -1,27 +1,95 @@
-//MangaInfo.java class
-public class MangaInfo {
-    public static String[] getMangaTitles() {
+public class MangaInfo extends Manga {
+    @Override
+    protected String[] getMangaTitles() {
         String[] mangaTitles = {
                 "Hajime no Ippo",
-                "Slam dunk",
+                "Slam Dunk",
                 "Angel Densetsu",
                 "Holyland",
-                "Grand blue",
+                "Grand Blue",
                 "Ashita no Joe",
-                "Kokou no Hito"
+                "Kokou no Hito",
+                "One Piece",
+                "Naruto",
+                "Bleach",
+                "Attack on Titan",
+                "Death Note",
+                "Fullmetal Alchemist",
+                "One Punch Man",
+                "My Hero Academia",
+                "Tokyo Ghoul",
+                "Fairy Tail",
+                "Dragon Ball",
+                "Black Clover",
+                "Demon Slayer: Kimetsu no Yaiba",
+                "Haikyu!!",
+                "Gintama",
+                "JoJo's Bizarre Adventure",
+                "Attack on Titan",
+                "One Punch Man",
+                "Kimi no Na wa",
+                "Koe no Katachi",
+                "Promised Neverland",
+                "Sword Art Online",
+                "Fate/stay night",
+                "Noragami",
+                "Vinland Saga",
+                "Beastars",
+                "Yakusoku no Neverland",
+                "Shingeki no Kyojin",
+                "Code Geass",
+                "Hunter x Hunter",
+                "Toradora!",
+                "Kaguya-sama: Love is War"
+
+
         };
         return mangaTitles;
     }
 
-    public static double[] getMangaPrices() {
+    @Override
+    protected double[] getMangaPrices() {
         double[] mangaPrices = {
                 10.99,   // Hajime no Ippo
-                8.99,    // Slam dunk
+                8.99,    // Slam Dunk
                 12.99,   // Angel Densetsu
                 9.99,    // Holyland
-                11.99,   // Grand blue
+                11.99,   // Grand Blue
                 7.99,    // Ashita no Joe
-                10.99    // Kokou no Hito
+                10.99,   // Kokou no Hito
+                8.99,    // One Piece
+                12.99,   // Naruto
+                9.99,    // Bleach
+                11.99,   // Attack on Titan
+                7.99,    // Death Note
+                10.99,   // Fullmetal Alchemist
+                8.99,    // One Punch Man
+                12.99,   // My Hero Academia
+                9.99,    // Tokyo Ghoul
+                11.99,   // Fairy Tail
+                9.99,    // Dragon Ball
+                10.49,   // Black Clover
+                9.49,    // Demon Slayer: Kimetsu no Yaiba
+                11.49,   // Haikyu!!
+                8.49,    // Gintama
+                10.49,   // JoJo's Bizarre Adventure
+                7.49,    // Attack on Titan
+                10.49,   // One Punch Man
+                9.49,    // Kimi no Na wa
+                11.49,   // Koe no Katachi
+                8.49,    // Promised Neverland
+                10.49,   // Sword Art Online
+                9.49,    // Fate/stay night
+                11.49,   // Noragami
+                9.49,    // Vinland Saga
+                10.49,   // Beastars
+                8.49,    // Yakusoku no Neverland
+                12.49,   // Shingeki no Kyojin
+                9.49,    // Code Geass
+                11.49,   // Hunter x Hunter
+                7.49,    // Toradora!
+                10.49    // Kaguya-sama: Love is War
+
         };
         return mangaPrices;
     }
