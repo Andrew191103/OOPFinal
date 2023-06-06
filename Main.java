6baa865dd2d16d49ccc77ff173827018837a0859
Andrew191103 <114073048+Andrew191103@users.noreply.github.com>
2023-06-06 10:47:43 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
-public class Main {
+public class Main { //The Main class serves as the entry point for the program.
     public static void main(String[] args) {
-        MangaList mangaList = new MangaList();
-        mangaList.printMangaInfo();
+        MangaList mangaList = new MangaList();  // Create an instance of MangaList
+        mangaList.printMangaInfo();  // Call the printMangaInfo() method to display manga information
     }
 }
+//In the main method, an instance of the MangaList class is created and assigned to the mangaList variable.
+//The MangaList class is assumed to have a printMangaInfo method that is called on the mangaList object.
+//The printMangaInfo method is responsible for displaying manga information, which might include titles, prices, or any other relevant details.
+//This code snippet demonstrates a basic usage of the MangaList class by creating an instance and invoking its printMangaInfo method.
