a65ca47324f7715761de15162de3acc07596ed29
Andrew191103 <114073048+Andrew191103@users.noreply.github.com>
2023-06-06 10:47:17 +0700

    Update Manga.java

--- Manga.java
+++ Manga.java
@@ -1,17 +1,18 @@
 public class Manga {
-    protected String[] mangaTitles;
-    protected double[] mangaPrices;
+    protected String[] mangaTitles;  // Array to store manga titles
+    protected double[] mangaPrices;  // Array to store manga prices
 
     public Manga() {
-        mangaTitles = getMangaTitles();
-        mangaPrices = getMangaPrices();
+        mangaTitles = getMangaTitles();  // Initialize mangaTitles array
+        mangaPrices = getMangaPrices();  // Initialize mangaPrices array
     }
 
     protected String[] getMangaTitles() {
-        return new String[0];
+        return new String[0];  // Default implementation returns an empty array
     }
 
     protected double[] getMangaPrices() {
-        return new double[0];
+        return new double[0];  // Default implementation returns an empty array
+
     }
 }
